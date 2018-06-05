9c347d291791c2da298a2d87459b11041fb8f909
mislavlukac <mislav.lukac@axilis.com>
2018-06-05 23:17:11 +0200

    withQuery added

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const withQuery = () => {
-    console.log(1);
-};
+import React from 'react';
+import { Query } from 'react-apollo';
 
+const withQuery = ({ query, options }) => Component => (props) => {
+    const opts = options ? options(props) : {};
+    return (
+      <Query
+        query={query}
+        variables={opts.variables || {}}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return (<div> Loading...</div>);
+          else if (error) return (<div> Error...</div>);
+          return (
+            <Component {...data} {...props} />
+          );
+        }}
+      </Query>
+    );
+  };
 module.exports = withQuery
